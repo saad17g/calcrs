0b902a377eb3dc2c30ff1cd33a4909f275bd90ba
saad17g <saad.guessous.2001@gmail.com>
2024-03-15 03:23:43 +0100

    final calc

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,42 @@
+use std::env;
+use std::process;
+
+mod evaluator;
 mod lexer;
+mod parser;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Usage: calcrs <expression>");
+        process::exit(1);
+    }
+
+    let expression = &args[1];
+
+    let tokens = lexer::tokenize(expression);
+    let ast = match parser::parse(&tokens) {
+        Ok(ast) => ast,
+        Err(err) => {
+            eprintln!("Parsing error: {}", err);
+            process::exit(1);
+        }
+    };
+
+    let result = match evaluator::evaluate(ast) {
+        Ok(val) => val,
+        Err(err) => match err {
+            evaluator::EvaluationError::DivisionByZero => {
+                eprintln!("Evaluation error: Division by zero");
+                process::exit(1);
+            }
+            evaluator::EvaluationError::InvalidOperation => {
+                eprintln!("Evaluation error: Invalid operation");
+                process::exit(1);
+            }
+        },
+    };
+
+    println!("{}", result);
 }
